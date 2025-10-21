e64d565fb1dbbfd842330b074fc59e76e5e489e7
Cristian DavidSanta Posada <cristiandavidsantap30@gmail.com>
2025-10-21 14:52:18 -0500

    Fix: Problemas en deploy 1.1

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,7 @@ import { Logger } from 'winston';
 import { version } from '../package.json';
 import config from './config/config.js';
 import { convert } from './mapper/index.js';
-import routes from './routes/index.js';
+import routes from './routes/index';
 import { ServerOptions } from './types/ServerOptions';
 import {
   createFolders,
