64a3e6220af39387c57ea897c612dadff6314d13
Cristian DavidSanta Posada <cristiandavidsantap30@gmail.com>
2025-10-28 15:32:19 -0500

    Fix: Autoclose session connected

--- src/util/createSessionUtil.ts
+++ src/util/createSessionUtil.ts
@@ -77,6 +77,7 @@ export default class CreateSessionUtil {
           {
             session: session,
             phoneNumber: client.config.phone ?? null,
+            autoClose: 0,
             deviceName:
               client.config.phone == undefined // bug when using phone code this shouldn't be passed (https://github.com/wppconnect-team/wppconnect-server/issues/1687#issuecomment-2099357874)
                 ? client.config?.deviceName ||
